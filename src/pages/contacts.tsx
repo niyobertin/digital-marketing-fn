e3599dd693174a7810c7b380209b8e0f840a2389
niyobertin <niyonkurubbertin@gmail.com>
2024-10-01 18:12:45 +0200

    fix on landing page

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-
+import contactImage from '../assets/imagescontact.png'
 const Contacts:React.FC = () =>{
     return(
         <>
-        <div className="bg-[#233a2f] text-white pt-[4%] pb-[2%] pb-[4%] mb-[4%] sm:ml-[20%] ml-[2%] sm:mr-[20%] mr-[2%] text-white text-lg rounded-[30px]" id="contacts">
-            <div>
-                <h1 className="text-[#25FD54] font-bold text-5xl text-center pb-[2%]">Contact us </h1>
-            <div className="sm:ml-[10%] ml-[2%] sm:mr-[10%] mr-[2%]">
+        <h1 className="text-[#25FD54] font-bold text-5xl text-center pb-[2%]">Contact us </h1>
+        <div className="sm:flex block gap-8">
+        <div className="sm:ml-[8%] mr-[2%] w-[80%] sm:block hidden">
+                <img src={contactImage} alt="contacts" className="h-[50vh] rounded-[20px]" />
+            </div>
+        <div className="bg-[#233a2f] text-white pt-[4%] pb-[2%] pb-[4%] mb-[4%] sm:mr-[8%] mr-[2%] w-full text-white text-lg rounded-[30px]" id="contacts">
+            <div>     
+            <div className="sm:ml-[2%] ml-[2%] sm:mr-[2%] mr-[2%]">
             <div className="mt-2">
                    <input type="text" placeholder="Name" className="bg-[#233a2f] border-b-2 border-green-500 w-full focus:outline-none focus:border-transparen"/>
                 </div>
@@ -14,7 +18,7 @@ const Contacts:React.FC = () =>{
                    <input type="email" placeholder="Email" className="bg-[#233a2f] border-b-2 border-green-500 w-full focus:outline-none focus:border-transparen"/>
                 </div>
                 <div className="mt-2">
-                   <input type="number" placeholder="Phone number" className="bg-[#233a2f] border-b-2 border-green-500 w-full focus:outline-none focus:border-transparen"/> 
+                   <input type="tel" placeholder="Phone number" className="bg-[#233a2f] border-b-2 border-green-500 w-full focus:outline-none focus:border-transparen"/> 
                 </div>
                 <div className="mt-2">
                    <textarea name="message" placeholder="Message" id="" className="bg-[#233a2f] border-b-2 border-green-500 w-full focus:outline-none focus:border-transparen"></textarea> 
@@ -24,6 +28,8 @@ const Contacts:React.FC = () =>{
            </div>
             </div>
             </div>
+        </div>
+            
         </div>
         </>
     )
